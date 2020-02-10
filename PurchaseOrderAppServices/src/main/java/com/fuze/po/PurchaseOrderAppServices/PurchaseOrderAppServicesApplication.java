e9e25f973091f7d7792164d5fe699fa24785c77d
roshanhyd <58503809+roshanhyd@users.noreply.github.com>
2020-02-10 13:08:32 +0530

    Initial commit for FUZE services

---
+++ PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/PurchaseOrderAppServicesApplication.java
@@ -0,0 +1,13 @@
+package com.fuze.po.PurchaseOrderAppServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderAppServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderAppServicesApplication.class, args);
+	}
+
+}
