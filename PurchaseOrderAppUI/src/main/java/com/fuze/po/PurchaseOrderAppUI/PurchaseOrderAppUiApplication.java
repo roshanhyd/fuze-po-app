d01a6b07bab22df3a90f31a83181964c2e01ddb8
roshanhyd <58503809+roshanhyd@users.noreply.github.com>
2020-02-10 13:05:16 +0530

    Initial Commit for UI project

---
+++ PurchaseOrderAppUI/src/main/java/com/fuze/po/PurchaseOrderAppUI/PurchaseOrderAppUiApplication.java
@@ -0,0 +1,13 @@
+package com.fuze.po.PurchaseOrderAppUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderAppUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderAppUiApplication.class, args);
+	}
+
+}
