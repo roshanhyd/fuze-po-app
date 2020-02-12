eebde5e4e7bd3d6e174cb703778e0f3bf79d1dea
Bhajuram.c <Bhajuram.c@BRILAP1027.BRILLIO.COM>
2020-02-12 17:12:45 +0530

    Modified cart database object.

--- PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/entity/Cart.java
+++ PurchaseOrderAppServices/src/main/java/com/fuze/po/PurchaseOrderAppServices/entity/Cart.java
@@ -5,8 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,17 +14,11 @@ public class Cart {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name = "market_id")
+	private String marketId;
 	@Column(name = "project_id")
 	private int projectId;
-	@ManyToOne
-	@JoinColumn(name = "item_id")
-	private Item item;
-	@Column(name = "quantity")
-	private int quantity;
-	@Column(name = "price")
-	private String price;
-	@Column(name = "added_by")
-	private String addedBy;
+	
 	public int getId() {
 		return id;
 	}
@@ -39,30 +31,11 @@ public class Cart {
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
-	
-	public Item getItem() {
-		return item;
+	public String getMarketId() {
+		return marketId;
 	}
-	public void setItem(Item item) {
-		this.item = item;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public String getPrice() {
-		return price;
-	}
-	public void setPrice(String price) {
-		this.price = price;
-	}
-	public String getAddedBy() {
-		return addedBy;
-	}
-	public void setAddedBy(String addedBy) {
-		this.addedBy = addedBy;
+	public void setMarketId(String marketId) {
+		this.marketId = marketId;
 	}
 	
 }
